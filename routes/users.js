b0bdd05a61c1bbaa03ef68487bc7f92f5d9841a9
Hamza Ahmad <hamzahmd.dev@gmail.com>
2021-08-06 11:28:23 +0500

    Saved a user on DB with hash bcrypt password

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
 
@@ -11,19 +12,42 @@ const User = require('../models/User');
 router.post(
   '/',
   [
-    body('name', 'Please add a name').not().isEmpty(),
-    body('email', 'Please add a valid email').isEmail(),
-    body('password', 'Please enter your password with 6+ char').isLength({
+    check('name', 'Please add a name').not().isEmpty(),
+    check('email', 'Please add a valid email').isEmail(),
+    check('password', 'Please enter your password with 6+ char').isLength({
       min: 6,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('passed');
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ msg: 'User Already Exists' });
+      }
+
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      //Assigning hash password
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+      res.send('User saved');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
   }
 );
 
