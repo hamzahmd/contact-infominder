381ca5078a46003cea8538dcee16c5d7d0c08d84
Hamza Ahmad <hamzahmd.dev@gmail.com>
2021-08-11 20:24:44 +0500

    production debugging env

--- config/db.js
+++ config/db.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+// const config = require('config');
+const db = process.env.MONGODB_URI;
 
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
       useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
     });
     console.log('MongoDB Connected');
   } catch (err) {
